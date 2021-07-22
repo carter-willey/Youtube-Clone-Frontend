caed3055db5bb2a951155aa1b8a5e5879a8fe495
carter-willey <cjwilley23@gmail.com>
2021-07-22 14:19:19 -0500

    added navbar component to main page

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import NavBar from './Components/NavBar/navBar';
+import axios from 'axios'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+
+  componentDidMount(){
+   this.getPopularVideos() 
+  }
+
+  getPopularVideos = async () => {
+    let response = await axios.get("https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=10&regionCode=US&key=AIzaSyCyZ0D0SYyRBDBvrkaYNVEJJSpCOIz_MVM")
+    console.log(response);
+  }
+
+  render() { 
+    return ( 
+      <div className="container">
+      <NavBar />
+    </div>
+     );
+  }
+}
+ 
+export default App;
